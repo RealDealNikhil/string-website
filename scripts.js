d358963047f28d5112420ecc79f3f6c59e437e42
Nikhil Suri <nikhilsuri@comcast.net>
2018-02-02 21:35:50 -0500

    working model

--- scripts.js
+++ scripts.js
@@ -1,17 +1,40 @@
 const LENGTH = 26;
+var arr = new Array(26);
+
+function reverse_count(str) {
+  var new_str = "";
+
+}
 
 function operate(str) {
   var reversed = str.split("").reverse().join("");
-  var arr = new Array(26);
-  $("#data").html(arr + ", " + str + ", " + reversed);
+  var c;
+  var index;
+  for (var i = 0; i < reversed.length; i++) {
+    c = reversed.charAt(i).toLowerCase();
+    index = c.charCodeAt(0) - 97;
+    if (arr[index] === undefined) {
+      arr[index] = 0;
+    }
+    arr[index]++;
+  }
+
+  // get unique characters
+  var unique = "";
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== undefined) {
+      unique += String.fromCharCode(i + 97) + " = " + arr[i] + "<br>";
+    }
+  }
+  $("#data").html(arr + "<br>" + str + "<br>" + reversed + "<br>" + unique);
   console.log(arr);
   console.log(str);
   console.log(reversed);
 }
 
 $(document).ready(function() {
-    $('#str-btn').click(function() {
-      operate($('#str').val());
-      return false;
-    });
+  $('#str-btn').click(function() {
+    operate($('#str').val());
+    return false;
+  });
 });
