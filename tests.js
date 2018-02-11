aeeab2578cf03a5457b0bf10ee046537b2121a60
Nikhil Suri <nikhilsuri@comcast.net>
2018-02-11 14:33:35 -0500

    object equality comparison

--- tests.js
+++ tests.js
@@ -1,10 +1,26 @@
+// store test results
 var results = {
   total: 0,
   failed: 0
 };
 
+// simple object equality comparison
+function isEqual(a, b) {
+  a_props = Object.getOwnPropertyNames(a);
+  b_props = Object.getOwnPropertyNames(b);
+  if (a_props.length != b_props.length) {
+    return false;
+  }
+  for (var i = 0; i < a_props.length; i++) {
+    var prop = a_props[i];
+    if (a[prop] !== b[prop]) {
+        return false;
+    }
+  }
+  return true;
+}
 
-
+// general testing function
 function test(str, expected_rev, expected_count) {
   var result = reverse_count(str);
   var reversed = result.reversed;
